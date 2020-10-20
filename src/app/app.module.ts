b069ff6e4128c8cf3beee1274e054302ae8c88aa
Devin <devin.biggers@gmail.com>
2020-10-19 22:19:30 -0500

    Installing Angular Material, making better ui decisions, smaller modals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SkillsPopupComponent } from './thinking-talents-main/skills-popup/skills-popup.component';
-import { TeamChartComponent } from './thinking-talents-main/team-chart/team-chart.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {SkillsPopupComponent} from './thinking-talents-main/skills-popup/skills-popup.component';
+import {TeamChartComponent} from './thinking-talents-main/team-chart/team-chart.component';
 import {ThinkingTalentsMainComponent} from './thinking-talents-main/thinking-talents-main.component';
-import {ReactiveFormsModule} from '@angular/forms';
-import { DisplayTeammateComponent } from './thinking-talents-main/display-teammate/display-teammate.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {DisplayTeammateComponent} from './thinking-talents-main/display-teammate/display-teammate.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -22,9 +24,14 @@ import { DisplayTeammateComponent } from './thinking-talents-main/display-teamma
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    FormsModule,
     ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
