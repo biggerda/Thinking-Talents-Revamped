de7d227b8bc180e64a69afeef29391f39e88027b
Devin <devin.biggers@gmail.com>
2021-01-17 19:48:27 -0600

    Attempting to connect to MongoDB cluster again

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+let database;
 
 // Mocked Data
 // const fs = require('fs');
@@ -11,18 +12,29 @@ const http = require('http');
 // Here we find an appropriate database to connect to, defaulting to
 // localhost if we don't find one.
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://dbigz:iDtd2rh5DBpj7G@maincluster.6gn9e.mongodb.net/thinking-talents?retryWrites=true&w=majority";
-// const uri = process.env.MONGODB_URI || "mongodb+srv://dbigz:iDtd2rh5DBpj7G@maincluster.6gn9e.mongodb.net/thinking-talents?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("thinking-talents").collection("teams");
-  // perform actions on the collection object
-  client.close();
+const uri = "mongodb+srv://dbigz:VODPxPWtDyOuH9oK@maincluster.6gn9e.mongodb.net/thinking-talents?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+client.connect((err, client) => {
+  if (err) throw err;
+
+  database = client.db('thinking-talents').collection('teams');
+
+  /**
+   * Get port from environment and store in Express.
+   */
+  const port = process.env.PORT || '8080';
+  app.set('port', port);
+
+  /**
+   * Create HTTP server.
+   */
+  const server = http.createServer(app);
+
+  /**
+   * Listen on provided port, on all network interfaces.
+   */
+  server.listen(port, () => console.log(`Application is running at http://127.0.0.1:${port}`));
 });
-
-
-
-
 
 
 // const mongoose = require('mongoose');
@@ -39,22 +51,21 @@ const teamRouter = express.Router();
 
 // Enable CORS
 const cors = require('cors');
+const {ObjectId} = require("bson");
 app.use(cors())
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist/updated-thinking-talents')));
 
 // Router Setup
-teamRouter.route('/teams')
+teamRouter.route('/sample-team')
   .get((req, res) => {
-    Teams.findById('6001ff268f860db83b6c6f7b',(err, sampleTeam) => {
-      if (err) {
-        console.log(`ERR - GET /teams - findById ${err}`);
-        return res.send(err);
-      }
-
-      return res.json(sampleTeam);
-    })
+    database.find("60038ec44d8074c7415bd82c")
+      .toArray()
+      .then(results => {
+        console.log(results);
+        res.status(200).send(results[0]);
+      });
   });
 
 app.use('/api', teamRouter);
@@ -70,19 +81,3 @@ app.use('/api', teamRouter);
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/updated-thinking-talents/index.html'));
 });
-
-/**
- * Get port from environment and store in Express.
- */
-const port = process.env.PORT || '8080';
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-server.listen(port, () => console.log(`Application is running at http://127.0.0.1:${port}`));
