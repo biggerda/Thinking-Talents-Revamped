36b830821798cc4a2a5afa1288eb870a72db754b
Devin <devin.biggers@gmail.com>
2021-01-17 21:11:18 -0600

    Dynamically changing pathname so that application will pull from mongo from multiple spaces

--- src/app/services/team.service.ts
+++ src/app/services/team.service.ts
@@ -2,19 +2,20 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Team} from '../../entities/Team';
+import {Location} from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamService {
+  private readonly pathname;
 
-  // private teamUrl = 'http://127.0.0.1:8080/api/sample-team';  // URL to web api
-  private teamUrl = 'https://nwo-dev.herokuapp.com/api/sample-team';  // URL to web api
-
-  constructor(private readonly _http: HttpClient) {
+  constructor(private readonly _http: HttpClient, private _location: Location) {
+    this.pathname = this._location.path();
   }
 
   getSampleTeams(): Observable<Team> {
-    return this._http.get<Team>(this.teamUrl);
+    const sampleTeamApi = this.pathname + 'api/sample-team';
+    return this._http.get<Team>(sampleTeamApi);
   }
 }
